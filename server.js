0225770ef5deb4a9056f73d0376512d4a052c498
blechdom <blechdom@gmail.com>
2012-12-29 03:22:03 -0500

    added text-to-speech preview and ability to save and delete text-to-speech based on mongodb _id

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ io.sockets.on('connection', function (socket) {
 
 			var currentTime = new Date();
 			var newName = currentTime.getTime() + ".mp3";
-			var savePath = "./public/snd/ttsaudio/" + newName;
+			var savePath = savepublic + "snd/ttsaudio/" + newName;
 			var fileStream = fs.createWriteStream(savePath);
 			request(downloadfile).pipe(fileStream);  
     		
